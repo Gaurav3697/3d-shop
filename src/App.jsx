65aa3b3955e2a31f8917f15592f3175e3ff88eeb
Gaurav3697 <gm4063420@gmail.com>
2024-11-20 17:30:32 +0545

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CanvasModel from './canvas'
+import Customizer from './pages/customizer'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <Customizer/>
+      <CanvasModel/>
+    </>
+  )
+}
+
+export default App
---
+++ src/canvas/index.jsx
@@ -0,0 +1,19 @@
+import { Canvas } from "@react-three/fiber"
+import {Environment, Center} from "@react-three/drei"
+import Shirt from "./Shirt"
+
+const CanvasModel = () => {
+  return (
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <Environment preset="city"/>
+        <Center>
+          {/* <Shirt/> */}
+        </Center>
+      </Canvas>
+    </div>
+  )
+}
+
+export default CanvasModel
